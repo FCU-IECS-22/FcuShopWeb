56f45bf441c2ac83ff9ad2e8948aebdba4b5dd11
D0898027 <d0898027@o365.fcu.edu.tw>
2021-12-20 02:03:49 +0800

    Init

---
+++ src/page/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <navbar/>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+
+
+export default {
+  name: "Index",
+  data() {
+    return {
+    }
+  },
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
