63c38e3db9886b2671271b5fd42db05306b2a0a2
chengyu <yces4199600@gmail.com>
2021-12-24 05:18:09 +0800

    旋轉木馬新增圖片

---
+++ src/components/Carousel.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://cdn.kingstone.com.tw/resp/980x300_koboxmas12.avif" class="d-block w-100" width="80%" height="300">
+    </div>
+    <div class="carousel-item">
+      <img src="https://cdn.kingstone.com.tw/resp/980x300_a2112042.avif" class="d-block w-100" width="80%" height="300">
+    </div>
+    <div class="carousel-item">
+      <img src="https://cdn.kingstone.com.tw/resp/980x300_20211223diarydks.avif" class="d-block w-100" width="80%" height="300">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </button>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Carousel"
+  }
+</script>
+
+<style>
+.carousel-control-next, .carousel-control-prev{
+    width: 5%;
+    opacity:0.1;
+}
+</style>
