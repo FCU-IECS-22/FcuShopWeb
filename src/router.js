e9be5361946816245ccbbc0d68b08c5786c63828
D0898027 <d0898027@o365.fcu.edu.tw>
2021-12-24 02:12:18 +0800

    Deploy

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Admin from '@/page/Admin.vue'
 import Index from '@/page/Index.vue'
 
@@ -38,7 +38,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   linkExactActiveClass: 'active',
   routes
 })
